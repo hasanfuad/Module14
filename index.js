3f81a0c47312d0c5823be633d78af947794f9362
hasanfuad <hasanmdfuad@hotmail.com>
2021-06-11 22:49:24 +0600

    leap year

--- index.js
+++ index.js
@@ -7,3 +7,25 @@ function inchTofeet(inch){
 
 var convertedFt = Math.round(inchTofeet(80));
 console.log(convertedFt +" Ft");
+
+
+// let const 
+
+let vary = 23; //changable
+const vary2 = 33; //unchanglable
+
+// leap year 
+
+var year = 2025;
+
+if(year%400==0){
+    console.log("Leap year");
+}
+else if (year % 100 != 0 && year % 4 == 0){
+    console.log("Leap Year");
+}
+
+else{
+    console.log("Not Leap Year");
+}
+
